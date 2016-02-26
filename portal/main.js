3caaf71fc61a07a1d90598f7bf2b4d65cb01924e
Meredith Espinosa <goclonefilms@gmail.com>
2016-02-26 06:40:49 +0000

    Happy birthday, me!

--- portal/main.js
+++ portal/main.js
@@ -8,3 +8,8 @@ function message() {
     // Twilio.sendMessage("1-650-739-6355", "You should come to hack club, room 206 from 3:30 to 5 on Tuesdays. Message sent via Twilio -Meredith");
     alert("Inactive for now. Will implement later.");
 }
+
+function birthday() {
+    Twilio.sendMessage("1-650-739-6355", "Happy Birthday, Meredith!");
+    alert("Done! Thank you!");
+}
