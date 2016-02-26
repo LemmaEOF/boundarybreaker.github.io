2f6ba0f561cab00f58b58750ac389deb64e6acfd
Meredith Espinosa <goclonefilms@gmail.com>
2016-02-26 06:20:53 +0000

    No spamming my phone

--- portal/main.js
+++ portal/main.js
@@ -5,5 +5,6 @@ function message() {
     // Twilio.sendMessage("1-650-644-6123", "You should come to hack club, room 206 from 3:30 to 5 on Tuesdays. Message sent via Twilio -Meredith");
     // Twilio.sendMessage("1-650-518-1113", "You should come to hack club, room 206 from 3:30 to 5 on Tuesdays. Message sent via Twilio -Meredith");
     // Twilio.sendMessage("1-650-906-9031", "You should come to hack club, room 206 from 3:30 to 5 on Tuesdays. Message sent via Twilio -Meredith");
-    Twilio.sendMessage("1-650-739-6355", "You should come to hack club, room 206 from 3:30 to 5 on Tuesdays. Message sent via Twilio -Meredith");
+    // Twilio.sendMessage("1-650-739-6355", "You should come to hack club, room 206 from 3:30 to 5 on Tuesdays. Message sent via Twilio -Meredith");
+    alert("Inactive for now. Will implement later.");
 }
