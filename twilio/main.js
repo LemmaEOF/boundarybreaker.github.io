f12b8fe3ba46da74a97bd75c0f348b18d5b32c0b
Meredith Espinosa <goclonefilms@gmail.com>
2017-05-18 23:13:10 -0700

    Removed excess files, changed title

--- twilio/main.js
+++
@@ -1,9 +0,0 @@
-// JavaScript File
-/* global Twilio*/
-function message() {
-    // Twilio.sendMessage("1-650-464-3029", "You should come to hack club, room 206 from 3:30 to 5 on Tuesdays. Message sent via Twilio -Meredith");
-    // Twilio.sendMessage("1-650-644-6123", "You should come to hack club, room 206 from 3:30 to 5 on Tuesdays. Message sent via Twilio -Meredith");
-    // Twilio.sendMessage("1-650-518-1113", "You should come to hack club, room 206 from 3:30 to 5 on Tuesdays. Message sent via Twilio -Meredith");
-    // Twilio.sendMessage("1-650-906-9031", "You should come to hack club, room 206 from 3:30 to 5 on Tuesdays. Message sent via Twilio -Meredith");
-    Twilio.sendMessage("1-650-739-6355", "You should come to hack club, room 206 from 3:30 to 5 on Tuesdays. Message sent via Twilio -Meredith");
-}
